955fe2fcf13f1eb93fa836140134172c082553cf
altentaller <n.altentaller@gmail.com>
2021-05-13 13:53:37 +0700

    updated imgs and load more btn for works block

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,13 @@
-
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
+$(function () {
+  $(".works__item").slice(0, 3).show();
+  $("body").on('click', '.load-more', function (e) {
+    e.preventDefault();
+    $(".works__item:hidden").slice(0, 3).slideDown();
+    if ($(".works__item:hidden").length == 0) {
+      $(".load-more").css('visibility', 'hidden');
+    }
+    $('html,body').animate({
+      scrollTop: $(this).offset().bottom
+    },);
   });
+});
