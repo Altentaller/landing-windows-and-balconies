28579c26281924aac026d8c761d8ac6ce361a0fa
altentaller <n.altentaller@gmail.com>
2021-05-18 12:22:04 +0700

    added form handler, update styles for form

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,3 +27,27 @@ $(window).scroll(function() {
     $('.scrollup').fadeOut();
   }
 });
+
+
+/* send orders */
+$(function() {
+  $('input[name=tel]').mask('+7 (000) 000-00-00');
+});
+
+$(document).ready(function () {
+  $('form').submit(function () {
+      var formID = $(this).attr('id');
+      var formNm = $('#' + formID);
+      $.ajax({
+          type: 'POST',
+          url: 'mailer/smart.php', 
+          data: formNm.serialize(),
+          success: function (data) {
+            $(formNm).find("input").val("");
+            $(formNm).find('.form-thanks').fadeIn('slow');
+            $(formNm).find('p').fadeOut('slow');
+          }
+      });
+      return false;
+  });
+});
